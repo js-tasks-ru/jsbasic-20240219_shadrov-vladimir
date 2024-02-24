1ad80afabb5a82c2660e17731fb5baab3fcc1024
Vladimir Shadrov <shadrov_vladimir@mail.ru>
2024-02-24 13:25:49 +0300

    Решение задач к Занятию 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  const wordsArray = str
+    .split("-")
+    .map((word, index) =>
+      index === 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .filter((word) => word !== "-");
+  return wordsArray.join("");
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  let res = "";
+  const filtredUsers = users.filter((user) => user.age <= age);
+
+  filtredUsers.forEach((user, index, array) => {
+    const { name, balance } = user;
+    res += `${name}, ${balance}`;
+    if (index < array.length - 1) {
+      res += "\n";
+    }
+  });
+  return res;
 }
