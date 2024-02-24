ca7e351cb8b329bbc08bf4f7ca6009d46d56c922
Vladimir Shadrov <shadrov_vladimir@mail.ru>
2024-02-24 13:55:08 +0300

    Удалена фильтрация элементов в задаче 3 модуля 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,7 +3,6 @@ function camelize(str) {
     .split("-")
     .map((word, index) =>
       index === 0 ? word : word[0].toUpperCase() + word.slice(1)
-    )
-    .filter((word) => word !== "-");
+    );
   return wordsArray.join("");
 }
