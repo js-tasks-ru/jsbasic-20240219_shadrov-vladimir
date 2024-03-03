b6a1707c3da40db4b0b648ce20dbb9469ccd2899
Vladimir Shadrov <shadrov_vladimir@mail.ru>
2024-03-03 12:42:16 +0300

    Решены задачи к занятию 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector(".toggle-text-button");
+  const text = document.querySelector("#text");
+  let isHidden = false;
+
+  btn.addEventListener("click", () => {
+    isHidden = !isHidden;
+    text.hidden = isHidden;
+  });
 }
