f0b3e29c40c0c230a548dc7c2b8e10953169d7e0
Vladimir Shadrov <shadrov_vladimir@mail.ru>
2024-03-15 18:13:57 +0300

    Реализация компонента UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,65 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.createElem(this.createElemTemplate());
+    this.tableBody = this.elem.querySelector('[data-id="my-table"]');
+    this.createEventListeners();
   }
+  createElem(template) {
+    const elem = document.createElement("div");
+    elem.innerHTML = template;
+    return elem.firstElementChild;
+  }
+
+  createElemTemplate() {
+    return `
+      <table>
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody data-id="my-table">
+          ${this.createTableBody()}
+        </tbody>
+      </table>
+    `;
+  }
+  createTableBody() {
+    return this.rows
+      .map(
+        (row) => `<tr data-name="${row.name}">${this.createTableRow(row)}</tr>`
+      )
+      .join("");
+  }
+
+  createTableRow(rowData) {
+    let rowTemplate = Object.values(rowData)
+      .map((dataItem) => `<td>${dataItem}</td>`)
+      .join("");
+
+    rowTemplate += '<td><button data-delete="true">X</button></td>';
+
+    return rowTemplate;
+  }
+
+  createEventListeners() {
+    this.elem.addEventListener("click", this.tableBodyClickHandler);
+  }
+
+  tableBodyClickHandler = (event) => {
+    const rowForDeleteIndex = event.target.closest("tr").dataset.name;
+
+    if (!rowForDeleteIndex) {
+      return;
+    }
+
+    this.rows = this.rows.filter(({ name }) => name !== rowForDeleteIndex);
+    this.tableBody.innerHTML = this.createTableBody();
+  };
 }
