3db15762c1f35141897a28b7b28ad5840cae797a
Vladimir Shadrov <shadrov_vladimir@mail.ru>
2024-03-23 22:47:00 +0300

    Решена задача Cart

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -3,32 +3,61 @@ export default class Cart {
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
+    this.cartItems = [];
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product || product === null) {
+      return;
+    }
+
+    const productIndex = this.findProductIndex(product.id);
+
+    if (productIndex > -1) {
+      this.cartItems[productIndex].count++;
+    } else {
+      this.cartItems.push({ product, count: 1 });
+    }
+
+    this.onProductUpdate(this.cartItems[productIndex]);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    const productIndex = this.findProductIndex(productId);
+
+    if (productIndex === -1) {
+      return;
+    }
+
+    this.cartItems[productIndex].count += amount;
+
+    if (this.cartItems[productIndex].count === 0) {
+      this.cartItems.splice(productIndex, 1);
+    } else {
+      this.onProductUpdate(this.cartItems[productIndex]);
+    }
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((acc, item) => acc + item.count, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((acc, item) => acc + item.product.price * item.count, 0);
   }
 
   onProductUpdate(cartItem) {
     // реализуем в следующей задаче
 
     this.cartIcon.update(this);
+  }
+
+  findProductIndex(productId) {
+    return this.cartItems.findIndex(cartItem => cartItem.product.id === productId);
   }
 }
 
